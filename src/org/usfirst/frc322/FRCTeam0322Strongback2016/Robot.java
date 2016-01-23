941e8e03c905fe94e224e95692d1ade45a5ae996
Raa'Shaun H <CardcaptorRaaShaun@gmail.com>
2016-01-23 14:44:02 -0500

    This code will drive a basic robot with four motors and tank drive.

--- src/org/usfirst/frc322/FRCTeam0322Strongback2016/Robot.java
+++ src/org/usfirst/frc322/FRCTeam0322Strongback2016/Robot.java
@@ -2,13 +2,40 @@
 package org.usfirst.frc322.FRCTeam0322Strongback2016;
 
 import org.strongback.Strongback;
+import org.strongback.components.Motor;
+import org.strongback.components.ui.ContinuousRange;
+import org.strongback.components.ui.FlightStick;
+import org.strongback.drive.TankDrive;
+import org.strongback.hardware.Hardware;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
 
+	private static final int JOYSTICK_PORT = 1;
+	private static final int LF_MOTOR_PORT = 1;
+	private static final int LR_MOTOR_PORT = 2;
+	private static final int RF_MOTOR_PORT = 3;
+	private static final int RR_MOTOR_PORT = 4;
+
+	private TankDrive drive;
+	private ContinuousRange driveSpeed;
+	private ContinuousRange turnSpeed;
+	
     @Override
     public void robotInit() {
+    	Motor left = Motor.compose(Hardware.Motors.talon(LF_MOTOR_PORT),
+    								Hardware.Motors.talon(LR_MOTOR_PORT));
+    	Motor right = Motor.compose(Hardware.Motors.talon(RF_MOTOR_PORT),
+									Hardware.Motors.talon(RR_MOTOR_PORT)).invert();
+    	drive = new TankDrive(left, right);
+    	
+    	FlightStick driveStick = Hardware.HumanInterfaceDevices.logitechAttack3D(JOYSTICK_PORT);
+    	driveSpeed = driveStick.getPitch();
+    	turnSpeed = driveStick.getRoll().invert();
+    	
+    	Strongback.configure().recordNoEvents().recordNoData().initialize();
+
     }
 
     @Override
@@ -19,10 +46,12 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
+    	drive.arcade(driveSpeed.read(), turnSpeed.read());
     }
 
     @Override
     public void disabledInit() {
+    	drive.stop();
         // Tell Strongback that the robot is disabled so it can flush and kill commands.
         Strongback.disable();
     }
