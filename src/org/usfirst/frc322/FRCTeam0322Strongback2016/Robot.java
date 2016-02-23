e5ca138c91c02ca170beea29c5de46a57971300b
Raa'Shaun H. <CardcaptorRaaShaun@gmail.com>
2016-02-23 13:22:09 -0500

    Major Rewrite All commands necessary to run current robot in Strongback have been added.

--- src/org/usfirst/frc322/FRCTeam0322Strongback2016/Robot.java
+++ src/org/usfirst/frc322/FRCTeam0322Strongback2016/Robot.java
@@ -1,7 +1,16 @@
 /* Created Sat Jan 23 12:56:39 EST 2016 */
 package org.usfirst.frc322.FRCTeam0322Strongback2016;
 
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.livewindow.LiveWindowSendable;
+
 import org.strongback.Strongback;
+import org.strongback.SwitchReactor;
 import org.strongback.components.Motor;
 import org.strongback.components.ui.ContinuousRange;
 import org.strongback.components.ui.FlightStick;
@@ -10,14 +19,6 @@ import org.strongback.components.ThreeAxisAccelerometer;
 import org.strongback.components.AngleSensor;
 import org.strongback.drive.TankDrive;
 import org.strongback.hardware.Hardware;
-
-import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class Robot extends IterativeRobot {
 
@@ -28,55 +29,80 @@ public class Robot extends IterativeRobot {
 	private static final int LR_MOTOR_PORT = 1;
 	private static final int RF_MOTOR_PORT = 3;
 	private static final int RR_MOTOR_PORT = 2;
+	private static final int LEFT_BALL_SUCK = 4;
+	private static final int RIGHT_BALL_SUCK = 4;
+	/*
 	private static final int LEFT_ENCOODER_PORT_A = 0;
 	private static final int LEFT_ENCOODER_PORT_B = 1;
 	private static final int RIGHT_ENCOODER_PORT_A = 2;
 	private static final int RIGHT_ENCOODER_PORT_B = 3;
 	private static final double ENCOODER_PULSE_DISTANCE = 1.0;
+	*/
 	private static final SPI.Port ACCEL_PORT = SPI.Port.kOnboardCS1;
 	private static final Range ACCEL_RANGE = Range.k2G;
 	private static final SPI.Port GYRO_PORT = SPI.Port.kOnboardCS0;
 
-
-	private TankDrive drive;
-	private ContinuousRange leftSpeed;
-	private ContinuousRange rightSpeed;
+	private FlightStick leftDriveStick, rightDriveStick;
+	private Gamepad manipulatorStick;
+	private Motor ballSuckMotor;
+	
+	private TankDrive drivetrain;
+	private ContinuousRange leftSpeed, rightSpeed;
+	private SwitchReactor ballSuck, ballSpit, stopCollector;
 
 	private ThreeAxisAccelerometer accel;
 	private AngleSensor gyro;
-	private AngleSensor leftEncoder;
-	private AngleSensor rightEncoder;
+	//private AngleSensor leftEncoder, rightEncoder;
 	
-	public static CameraServer cameraServer;
+	public static CameraServer cameraServer;	
 	
-	
-    @Override
     public void robotInit() {
-    	Motor left = Motor.compose(Hardware.Motors.talon(LF_MOTOR_PORT),
-    								Hardware.Motors.talon(LR_MOTOR_PORT)).invert();
+    	//Setup drivetrain
+    	Motor leftDriveMotor = Motor.compose(Hardware.Motors.talon(LF_MOTOR_PORT),
+    											Hardware.Motors.talon(LR_MOTOR_PORT)).invert();
     	LiveWindow.addActuator("Left Front Motor", LF_MOTOR_PORT, (Talon) Hardware.Motors.talon(LF_MOTOR_PORT));
     	LiveWindow.addActuator("Left Rear Motor", LR_MOTOR_PORT, (Talon) Hardware.Motors.talon(LR_MOTOR_PORT));
-    	Motor right = Motor.compose(Hardware.Motors.talon(RF_MOTOR_PORT),
-									Hardware.Motors.talon(RR_MOTOR_PORT));
+    	Motor rightDriveMotor = Motor.compose(Hardware.Motors.talon(RF_MOTOR_PORT),
+    											Hardware.Motors.talon(RR_MOTOR_PORT));
     	LiveWindow.addActuator("Right Front Motor", RF_MOTOR_PORT, (Talon) Hardware.Motors.talon(RF_MOTOR_PORT));
     	LiveWindow.addActuator("Right Rear Motor", RR_MOTOR_PORT, (Talon) Hardware.Motors.talon(RR_MOTOR_PORT));
-    	drive = new TankDrive(left, right);
+    	drivetrain = new TankDrive(leftDriveMotor, rightDriveMotor);
     	
-    	FlightStick leftDriveStick = Hardware.HumanInterfaceDevices.logitechAttack3D(LEFT_DRIVESTICK_PORT);
-    	FlightStick rightDriveStick = Hardware.HumanInterfaceDevices.logitechAttack3D(RIGHT_DRIVESTICK_PORT);
-    	Gamepad manipulatorStick = xbox360(MANIPULATOR_STICK_PORT);
-    	leftSpeed = leftDriveStick.getPitch();
-    	rightSpeed = rightDriveStick.getPitch();
+    	//Setup manipulators
+    	ballSuckMotor = Motor.compose(Hardware.Motors.talon(LEFT_BALL_SUCK),
+    									Hardware.Motors.talon(RIGHT_BALL_SUCK).invert());
+    	LiveWindow.addActuator("Left Ball Suck", LF_MOTOR_PORT, (Talon) Hardware.Motors.talon(LEFT_BALL_SUCK));
+     	LiveWindow.addActuator("Right Ball Suck", LR_MOTOR_PORT, (Talon) Hardware.Motors.talon(RIGHT_BALL_SUCK));
+
+     	//Setup joysticks
+    	leftDriveStick = Hardware.HumanInterfaceDevices.logitechAttack3D(LEFT_DRIVESTICK_PORT);
+    	rightDriveStick = Hardware.HumanInterfaceDevices.logitechAttack3D(RIGHT_DRIVESTICK_PORT);
+    	//This is actually an Xbox 360 controller but, the F310 is close enough
+    	manipulatorStick = Hardware.HumanInterfaceDevices.logitechF310(MANIPULATOR_STICK_PORT);
     	
+    	//Setup sensors
     	accel = Hardware.Accelerometers.accelerometer(ACCEL_PORT, ACCEL_RANGE);
-    	//LiveWindow.addSensor("Accelerometer", 1, accel);
+    	//LiveWindow.addSensor("Accelerometer", 1, accel); //I have no idea how to add the Strongback Accelerometer to a LiveWindow
     	gyro = Hardware.AngleSensors.gyroscope(GYRO_PORT);
-    	//LiveWindow.addSensor("Gyroscope", 0, gyro);
+    	//LiveWindow.addSensor("Gyroscope", 0, gyro); //I have no idea how to add the Strongback Gyroscope to a LiveWindow
+
+    	//We weren't able to attach the encoder hardware properly so, I'm pulling them from the code until we figure it out
     	//leftEncoder = Hardware.AngleSensors.encoder(LEFT_ENCOODER_PORT_A, LEFT_ENCOODER_PORT_B, ENCOODER_PULSE_DISTANCE);
     	//rightEncoder = Hardware.AngleSensors.encoder(RIGHT_ENCOODER_PORT_A, RIGHT_ENCOODER_PORT_B, ENCOODER_PULSE_DISTANCE);    	
     	//LiveWindow.addSensor("Left Encoder", LEFT_ENCOODER_PORT_A, leftEncoder);
     	//LiveWindow.addSensor("Right Encoder", RIGHT_ENCOODER_PORT_A, rightEncoder);
     	
+    	//Setup drivetrain variables
+    	ContinuousRange sensitivity = leftDriveStick.getThrottle().map(t -> (t + 1.0) / 2.0);
+    	leftSpeed = leftDriveStick.getPitch().scale(sensitivity::read);
+    	rightSpeed = rightDriveStick.getPitch().scale(sensitivity::read);
+    	
+    	//Setup OI Buttons
+    	ballSuck = Strongback.switchReactor();
+    	ballSpit = Strongback.switchReactor();
+    	stopCollector = Strongback.switchReactor();
+    	
+    	//Setup Camera
     	cameraServer = CameraServer.getInstance();
         cameraServer.setQuality(25);
         cameraServer.startAutomaticCapture("cam0");
@@ -84,7 +110,6 @@ public class Robot extends IterativeRobot {
     	Strongback.configure().recordNoEvents().recordNoData().initialize();
     }
 
-    @Override
     public void autonomousInit() {
         // Start Strongback functions ...
         Strongback.start();
@@ -95,43 +120,27 @@ public class Robot extends IterativeRobot {
     	
     }
     
-    @Override
     public void teleopInit() {
         // Restart Strongback functions ...
         Strongback.restart();
     }
 
-    @Override
     public void teleopPeriodic() {
-    	drive.tank(leftSpeed.read(), rightSpeed.read());
+    	//This line runs the drivetrain
+    	drivetrain.tank(leftSpeed.read(), rightSpeed.read());
+    	//These two handle boulder pickup
+    	ballSuck.whileTriggered(manipulatorStick.getA(), ()->Strongback.submit(new SuckBall(ballSuckMotor)));
+    	ballSuck.whileUntriggered(manipulatorStick.getA(), ()->Strongback.submit(new StopCollector(ballSuckMotor)));
+    	//These two handle boulder release
+    	ballSpit.whileTriggered(manipulatorStick.getA(), ()->Strongback.submit(new SpitBall(ballSuckMotor)));
+    	ballSuck.whileUntriggered(manipulatorStick.getA(), ()->Strongback.submit(new StopCollector(ballSuckMotor)));
+    	//This one stops the collector
+    	stopCollector.whileTriggered(manipulatorStick.getLeftBumper(), ()->Strongback.submit(new StopCollector(ballSuckMotor)));
     }
 
-    @Override
     public void disabledInit() {
-    	drive.stop();
+    	drivetrain.stop();
         // Tell Strongback that the robot is disabled so it can flush and kill commands.
         Strongback.disable();
-    }
-    public static org.strongback.components.ui.Gamepad xbox360(int port) {
-    	Joystick joystick = new Joystick(port);
-    	return Gamepad.create(joystick::getRawAxis,
-    						  joystick::getRawButton,
-    						  joystick::getPOV,
-    						  () -> joystick.getRawAxis(0),
-    						  () -> joystick.getRawAxis(1),
-    						  () -> joystick.getRawAxis(3),
-    						  () -> joystick.getRawAxis(4),
-    						  () -> joystick.getRawAxis(2),
-    						  () -> joystick.getRawAxis(2),
-    						  () -> joystick.getRawButton(4),
-    						  () -> joystick.getRawButton(5),
-    						  () -> joystick.getRawButton(0),
-    						  () -> joystick.getRawButton(1),
-    						  () -> joystick.getRawButton(2),
-    						  () -> joystick.getRawButton(3),
-    						  () -> joystick.getRawButton(7),
-    						  () -> joystick.getRawButton(6),
-    						  () -> joystick.getRawButton(8),
-    						  () -> joystick.getRawButton(9));
-    }
+    }    
 }
---
+++ src/org/usfirst/frc322/FRCTeam0322Strongback2016/SuckBall.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc322.FRCTeam0322Strongback2016;
+
+import org.strongback.command.*;
+import org.strongback.components.Motor;
+
+public class SuckBall extends Command {
+	private final Motor motor;
+	
+	public SuckBall(Motor motor) {
+		super(motor);
+		this.motor = motor;
+	}
+	
+	public boolean execute() {
+		this.motor.setSpeed(1.0);
+		return true;
+	}
+}
